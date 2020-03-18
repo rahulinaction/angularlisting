df4507f85df433bc5805253aa93f6c08c71591c1
Rahul Mendonca <rahul.mendonca@resolver.com>
2020-03-18 16:02:09 -0400

    Adding components and routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-
-
-const routes: Routes = [];
+import {HomeComponent} from '../app/home/home.component';
+import {ListComponent} from '../app/list/list.component';
+const routes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'list', component: ListComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
